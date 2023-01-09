46c0205ed8a1e0b66008e2c4846089e1beb956b4
Alan Coomes <acoomes33@gmail.com>
2023-01-09 16:20:15 -0600

    finished mobile header styling

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import styles from "../Header/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <img
-        src="./assets/images/logo.svg"
-        alt="Logo that directs to Home Page"
-      />
-      <nav>
+    <header className={styles.header}>
+      <a className={styles.logoLink} href="#">
+        <img
+          src="./assets/images/logo.svg"
+          alt="Logo that directs to Home Page"
+        />
+      </a>
+
+      <nav className={styles.nav}>
         <div>Home</div>
         <div>New</div>
         <div>Popular</div>
         <div>Trending</div>
         <div>Categories</div>
       </nav>
+      <button className={styles.menu}>
+        <img src="/assets/images/icon-menu.svg" alt="" />
+      </button>
     </header>
   );
 };
